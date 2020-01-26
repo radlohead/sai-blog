607e3826bca6299cd487c8377caddf4f5d6aeadc
= <radlohead@naver.com>
2020-01-26 14:26:04 +0900

    Join - type 수정

--- client/src/containers/Join.tsx
+++ client/src/containers/Join.tsx
@@ -3,18 +3,34 @@ import { useForm } from 'react-hook-form'
 import axios from 'axios'
 import moment from 'moment'
 
+type TJoin = {
+    id: string
+    password: string
+}
+
 const Join = () => {
-    const { handleSubmit, register, errors }: any = useForm()
-    const onSubmit = (values: any) => {
+    const { handleSubmit, register, errors } = useForm()
+    const onSubmit: any = async ({ id, password }: TJoin) => {
+        const instance = axios.create({
+            baseURL: 'http://localhost:4000',
+            timeout: 5000
+        })
         const date = moment()
             .format()
             .substr(0, 19)
-        axios.post('http://localhost:4000/join', {
-            id: values.id,
-            password: values.password,
-            createdAt: date,
-            updatedAt: date
-        })
+        try {
+            const responseData = await instance.post('/join', {
+                id,
+                password,
+                createdAt: date,
+                updatedAt: date
+            })
+            alert('회원가입이 완료되었습니다.')
+            return responseData
+        } catch (err) {
+            alert('회원가입이 실패했습니다.')
+            console.error(err)
+        }
     }
 
     return (
@@ -23,26 +39,25 @@ const Join = () => {
                 name="id"
                 ref={register({
                     required: 'Required',
-                    pattern: {
-                        value: /[a-z0-9]{2,20}/i,
-                        message:
-                            '아이디는 2글자 이상, 20글자 이하로 입력해주세요.'
-                    }
+                    minLength: 2,
+                    maxLength: 20
                 })}
             />
-            {errors.id && errors.id.message}
+            {errors.id && (
+                <span>아이디는 2글자 이상 20글자 이하로 입력해주세요.</span>
+            )}
 
             <input
                 name="password"
                 ref={register({
                     required: 'Required',
-                    pattern: {
-                        value: /.{4,}/,
-                        message: '비밀번호는 4글자 이상 입력해주세요.'
-                    }
+                    minLength: 4,
+                    maxLength: 20
                 })}
             />
-            {errors.password && errors.password.message}
+            {errors.password && (
+                <span>비밀번호는 4글자 이상 20글자 이하로 입력해주세요.</span>
+            )}
 
             <button type="submit">회원가입</button>
         </form>
