ca1baa29144d734b7ff74fd81df96a39215244c3
= <radlohead@naver.com>
2020-01-26 14:46:55 +0900

    router 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Link, Route } from 'react-router-dom'
 import './App.css'
 import Join from './containers/Join'
 
 const App: React.FC = () => {
     return (
         <div className="App">
-            <Join />
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/join">회원가입</Link>
+                    </li>
+                </ul>
+            </nav>
+            <div className="container">
+                <Route path="/" exact />
+                <Route path="/join" component={Join} />
+            </div>
         </div>
     )
 }
