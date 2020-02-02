e51af6525c5c5b09e79f3291d0eb9f433a3db767
= <radlohead@naver.com>
2020-02-02 14:14:32 +0900

    api 주소 production 구분해서 설정

--- client/src/components/Common/Constants/index.ts
+++ client/src/components/Common/Constants/index.ts
@@ -1,4 +1,5 @@
-const S3_URL = 'http://sai-blog.s3-website.ap-northeast-2.amazonaws.com/'
+const EBS_URL =
+    'http://nodeblog-env.ccc35uvu2d.ap-northeast-2.elasticbeanstalk.com'
 const LOCAL_SERVER_URL = 'http://localhost:4000'
 export const BASE_URL =
-    window.location.href === S3_URL ? S3_URL : LOCAL_SERVER_URL
+    process.env.NODE_ENV === 'production' ? EBS_URL : LOCAL_SERVER_URL
