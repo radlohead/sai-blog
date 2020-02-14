cd2b548cc83aedd6d95cfa8f3706cd80fd6430a7
= <radlohead@naver.com>
2020-02-14 10:26:10 +0900

    최근글 클릭시 전체 포스트 불러오기

--- client/src/components/Board/BoardList.tsx
+++ client/src/components/Board/BoardList.tsx
@@ -37,7 +37,7 @@ const BoardList = (props: RouteComponentProps<{ rowId: string }>) => {
     }
     useEffect(() => {
         fetchBoardList()
-    }, [])
+    }, [!props.location.state])
     const fetchSelectedItemList = async () => {
         const instance = axios.create({
             baseURL: BASE_URL,
