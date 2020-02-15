86471dc8deda74e943e6c77f96345ef6120a0ebc
= <radlohead@naver.com>
2020-02-15 14:57:24 +0900

    비로그인시에도 전체 포스팅 기본 노출로 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,41 @@
 import React, { useEffect } from 'react'
 import { Link, Route, useHistory } from 'react-router-dom'
-import Home from './containers/Home'
 import Join from './containers/Join'
 import Board from './containers/Board'
 import './style/App.scss'
 
 const App: React.FC = () => {
-    const history = useHistory()
     const getUserInfo = localStorage.getItem('sai-blog')
-
+    const history = useHistory()
     useEffect(() => {
-        if (getUserInfo) history.push('/board')
-    })
+        history.push({
+            pathname: '/board',
+            state: { total: true }
+        })
+    }, [history])
 
     return (
         <div className="App">
-            {!getUserInfo && (
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
+            <nav>
+                <ul>
+                    <li>
+                        <Link
+                            to={{
+                                pathname: '/board',
+                                state: { total: true }
+                            }}
+                        >
+                            전체 글보기
+                        </Link>
+                    </li>
+                    {!getUserInfo && (
                         <li>
                             <Link to="/join">회원가입</Link>
                         </li>
-                        <li>
-                            <Link
-                                to={{
-                                    pathname: '/board',
-                                    state: { total: true }
-                                }}
-                            >
-                                최근글
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-            )}
+                    )}
+                </ul>
+            </nav>
             <div className="container">
-                <Route path="/" exact component={Home} />
                 <Route path="/join" component={Join} />
                 <Route path="/board" component={Board} />
             </div>
